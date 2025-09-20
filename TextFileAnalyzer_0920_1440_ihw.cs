7fa00e505ed11d6cfb7363d4630190b566ceae97
nova1147 <tetra.00aero@icloud.com>
2025-09-20 07:40:58 +0100

    Enhance 文本文件内容分析器 using ASPNET

---
+++ TextFileAnalyzer_0920_1440_ihw.cs
@@ -0,0 +1,14 @@
+// 代码生成时间: 2025-09-20 14:40:57
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TextFileAnalyzer
+{
+    /// <summary>
+    /// Controller for text file analysis.
+    /// </summary>
+    [ApiController]
+    [Route("[controller]/[action]
